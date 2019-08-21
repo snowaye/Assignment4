ddf842214a721d851a81cab76bec78dba189fcfc
Mobile Dev 2 <mobiledev2@Mobiles-iMac-2.local>
2019-08-21 17:32:21 +0630

    Adding Fragments and BottomNavigationView

--- app/src/main/java/padc/batch9/assignment4/MainActivity.java
+++ app/src/main/java/padc/batch9/assignment4/MainActivity.java
@@ -2,28 +2,49 @@ package padc.batch9.assignment4;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.AppCompatTextView;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toolbar;
+
+import padc.batch9.assignment4.fragment.FragmentOne;
+import padc.batch9.assignment4.fragment.FragmentThree;
+import padc.batch9.assignment4.fragment.FragmentTwo;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
 
+    public static FragmentManager fragmentManager;
+
+    BottomNavigationView bNavView;
+    public static AppCompatTextView tvTitle;
+    Toolbar toolbar;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    fragment = new FragmentOne();
+                    setTitle("Home");
+                    loadFragment(fragment);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    fragment = new FragmentTwo();
+                    setTitle("Search");
+                    loadFragment(fragment);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    fragment = new FragmentThree();
+                    setTitle("Time");
+                    loadFragment(fragment);
                     return true;
             }
             return false;
@@ -34,9 +55,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        initializeData();
+        initializeUiComponent();
+
+        bNavView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void initializeData() {
+        fragmentManager = getSupportFragmentManager();
+    }
+
+    private void initializeUiComponent() {
+        bNavView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        tvTitle = toolbar.findViewById(R.id.tv_title);
+    }
+
+    public static void loadFragment(Fragment fragment) {
+        // load com.mytel.msg.fragment
+        FragmentTransaction transaction;
+        transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    public static void setTitle(String title) {
+        tvTitle.setText(title);
     }
 
 }
---
+++ app/src/main/java/padc/batch9/assignment4/fragment/FragmentTwo.java
@@ -0,0 +1,21 @@
+package padc.batch9.assignment4.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentTwo extends Fragment {
+
+    public FragmentTwo() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
